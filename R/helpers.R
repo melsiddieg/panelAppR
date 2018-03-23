b33ebf9f9d5185492a1025913950c557980cb27f
melsiddieg <melsiddieg@gmail.com>
2018-03-23 15:13:38 +0200

    added support for null values in filters

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,5 @@
 get_qparams <- function(params){
+  params <- params %>% compact %>% keep(~nchar(.x)>1)
   paramsVec <- c()
   for(p in seq_along(params)){
     paramsVec <- append(paramsVec, paste0(names(params)[p], "=", params[p]))
